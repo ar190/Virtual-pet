3a99bc854a5d527f2f9c46738ed4bfdf985a1c6e
ar190 <69691425+ar190@users.noreply.github.com>
2020-10-26 19:52:51 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 var dog,dogImg,dogImg1;
 var database;
 var foodS,foodStock;
+var fedTime,lastFed;
+var FoodObj;
 
 function preload(){
    dogImg=loadImage("Images/Dog.png");
@@ -10,47 +12,69 @@ function preload(){
 //Function to set initial environment
 function setup() {
   database=firebase.database();
-  createCanvas(500,500);
+  createCanvas(1000,400);
 
-  dog=createSprite(250,300,150,150);
+  foodObj = Newfood();
+
+
+  dog=createSprite(800,200,150,150);
   dog.addImage(dogImg);
   dog.scale=0.15;
 
   foodStock=database.ref('Food');
   foodStock.on("value",readStock);
-  textSize(20); 
+  textSize(20);
+  feed=createButton("Feed the Dog");
+  feed.postion(700,95);
+  feed.mousePressed(feedDog);
+  
+  addFood=createButton("Add Food");
+  addFood.postion(800,95);
+  addFood.mousePressed(addFoods);
 }
 
 // function to display UI
 function draw() {
   background(46,139,87);
- 
-  if(keyWentDown(UP_ARROW)){
-    writeStock(foodS);
-    dog.addImage(dogImg1);
-  }
+ foodObj.Display();
 
-  drawSprites();
-  fill(255,255,254);
-  stroke("black");
-  text("Food remaining : "+foodS,170,200);
-  textSize(13);
-  text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
+ fedTime=database.ref('FeedTime');
+ fedTime.on("value",function(data){
+   lastFed=data.val();
+ });
+
+ fill(255,255,254);
+ textSize(15);
+ if(lastFed>=12){
+   text("Last Feed :" +lastFed%12 + PM ,350,30);
+ } else if (lastFed==0){
+  text("Last Feed : 12 AM",350,30);
+ }else{
+  text("Last Feed :"+lastFed + AM ,350,30);
+
+ }
 }
-
-//Function to read values from DB
+drawSprites();
+ 
+  //Function to read values from DB
 function readStock(data){
   foodS=data.val();
+  foodObj.updateFoodStock(foodS);
 }
 
 //Function to write values in DB
-function writeStock(x){
-  if(x<=0){
-    x=0;
-  }else{
-    x=x-1;
-  } 
+function feedDog(){
+  dog.addImage(dogImg1);
+  foodObj.updateFoodStock(foodObj.getFoodSock()-1);
   database.ref('/').update({
-    Food:x
+    food:foodObj.getFoodSock(),
+    FeedTime:hour()
+  })
+}
+
+function addFood(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
   })
 }
